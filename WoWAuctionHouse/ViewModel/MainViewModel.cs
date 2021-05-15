e069cda606c57c969a30de61cd490dcdbf41ff1d
Dariusz Sobolewski <dejvis1994@gmail.com>
2021-05-15 12:38:22 +0200

    generate update

--- WoWAuctionHouse/ViewModel/MainViewModel.cs
+++ WoWAuctionHouse/ViewModel/MainViewModel.cs
@@ -1,4 +1,6 @@
 using GalaSoft.MvvmLight;
+using Squirrel;
+using System.Threading.Tasks;
 
 namespace WoWAuctionHouse.ViewModel
 {
@@ -6,6 +8,32 @@ namespace WoWAuctionHouse.ViewModel
     {
         public MainViewModel()
         {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+
+            WindowTitle = $"Auction House {version}";
+
+            Task.Run(async () => await Update());
+        }
+
+        private string _windowTitle;
+
+        public string WindowTitle
+        {
+            get => _windowTitle;
+            set
+            {
+                Set(() => WindowTitle, ref _windowTitle, value);
+            }
+        }
+
+        public async Task Update()
+        {
+            using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/Dejvis000/WorldOfWarcraftAuctionHouse", prerelease:true))
+            {
+                await mgr.Result.UpdateApp();
+            }
         }
 
     }
